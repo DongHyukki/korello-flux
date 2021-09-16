085dc25a84cd43e9f230a7bc986f140b2dc5af28
CoDingDongHyuk <kderr2791@gmail.com>
2021-09-16 15:43:26 +0900

    fix: Test 주석 처리

--- src/test/kotlin/com/hyuki/korelloflux/KorelloFluxApplicationTests.kt
+++ src/test/kotlin/com/hyuki/korelloflux/KorelloFluxApplicationTests.kt
@@ -6,16 +6,16 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class KorelloFluxApplicationTests {
-
-    @Autowired
-    lateinit var eventService: EventService
-
-    @Test
-    fun contextLoads() {
-        eventService.saveEvent(Event("1","2","3",4,"5","6"))
-        println("11")
-    }
-
-}
+//@SpringBootTest
+//class KorelloFluxApplicationTests {
+//
+//    @Autowired
+//    lateinit var eventService: EventService
+//
+//    @Test
+//    fun contextLoads() {
+//        eventService.saveEvent(Event("1","2","3",4,"5","6"))
+//        println("11")
+//    }
+//
+//}
